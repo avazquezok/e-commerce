b4b3968131e93dd41340f806c7e37642577e4f7f
dullkarlmarx <agustinvazquezrocha33@gmail.com>
2022-08-19 18:07:16 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -5,7 +5,7 @@ const email = document.getElementById("email");
 login.addEventListener("click", () => {
     if (password!==null && email!==null) {
         alert("success");
-        window.location.href = "http://www.w3schools.com";
+        window.location.href = "https://dullkarlmarx.github.io/e-comerce/principal.html";
     } else {
         alert("error");
     }
