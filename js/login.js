d82b98950e36ca4c06e7d913ddf3662e5bebabe2
dullkarlmarx <agustinvazquezrocha33@gmail.com>
2022-08-30 21:05:58 -0300

    login update

--- js/login.js
+++ js/login.js
@@ -2,14 +2,15 @@ const email = document.getElementById("email");
 const password = document.getElementById("password");
 const login = document.getElementById("login");
 
-login.addEventListener("click", () => {
+login.addEventListener("click", (e) => {
+e.preventDefault();
     if (email.value.length == 0) {
         alert("error email must be at least one character");
     }
     else if (password.value.length == 0) {
         alert("error password must be at least one character");
     } else {
-        alert("success");
-        window.location = "principal.html";
+        console.log(email.value);
+      window.location = "principal.html";
     }
 });
