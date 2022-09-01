a27be3a1ca512e60e76fc58f97bcfad496dc6a20
dullkarlmarx <agustinvazquezrocha33@gmail.com>
2022-09-01 17:29:01 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,15 +1,21 @@
-document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("autos").addEventListener("click", function() {
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("autos").addEventListener("click", function () {
         localStorage.setItem("catID", 101);
         window.location = "products.html"
     });
-    document.getElementById("juguetes").addEventListener("click", function() {
+    document.getElementById("juguetes").addEventListener("click", function () {
         localStorage.setItem("catID", 102);
         window.location = "products.html"
     });
-    document.getElementById("muebles").addEventListener("click", function() {
+    document.getElementById("muebles").addEventListener("click", function () {
         localStorage.setItem("catID", 103);
         window.location = "products.html"
     });
 });
-localStorage.getItem("email");
+
+let userEmail = localStorage.getItem("email");
+let container = document.getElementById("container");
+
+document.addEventListener("DOMContentLoaded", function () {
+    container.innerHTML += `<p>${userEmail}</p>`;
+})
